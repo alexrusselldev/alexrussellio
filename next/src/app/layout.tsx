a6b4dca682cacdae3cd1558f8821413c7d987599
Alex Russell <alexrusselldev@gmail.com>
2023-05-28 21:15:17 +0100

    feat: ✨ add header to root layout

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -1,21 +1,20 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+import Header from "../components/Header/Header";
 
 export const metadata = {
-  title: 'Alex Russell',
-  description: 'Full stack web developer.',
-}
+  title: "Alex Russell",
+  description: "Full stack web developer.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Header />
+      <body className="bg-gruv-bg-2">{children}</body>
     </html>
-  )
+  );
 }
